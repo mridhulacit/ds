911c13613e477f0b88dc0618daf3e5cfa6fc2a13
CodeExplorer <pmcreddy@gmail.com>
2018-11-09 12:55:05 +0530

    constructor overloaded

---
+++ c++/const.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+class one
+{
+    public:
+    int a,b;
+    public:
+    one()
+    {
+        cout<<"this is non parametrized default constructor"<<endl;
+        a=0;
+        b=0;
+    }
+    one(int x,int y)
+    {
+        cout<<"this is parametrized constructor"<<endl;
+        a=x;
+        b=y;
+    }
+    one(int x)
+    {
+        cout<<"this is also parametrized constructor with one argument"<<endl;
+         a=x;
+    }
+    one(one &i) //noe the object given in its parameter is checked and the matched ones value willl be copied to the respective variables//
+    {
+        cout<<"this is copy constructor"<<endl;
+        a=i.a;
+        b=i.b;
+    }
+    public:
+    void display()
+    {
+        cout<<"the value of a is"<<a<<endl;
+        cout<<"the value of b is"<<b<<endl;
+    }
+};
+int main()
+{
+    one m(10,20); //parametrized constructor is called//
+    m.display(); 
+    one w(12); //parametrized constructor with one argument is called//
+    w.display();
+    one v(m); //here the copy constructor is called//
+    //the value of object m is copied to object v//
+    v.display();
+    one c; //non parametrized constructor is called//
+    c.display();
+    //for each object the respective function is called with the object and dot operator//
+    return 0;
+
+}
