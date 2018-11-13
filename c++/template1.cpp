e2b9b88f30ce7aea0c081c66fcfb3782cf8ac212
CodeExplorer <pmcreddy@gmail.com>
2018-11-13 19:28:19 +0530

    simple template prog

--- c++/template1.cpp
+++ c++/template1.cpp
@@ -1,27 +1,42 @@
 #include<iostream>
 using namespace std;
 template <class T>
-class Addition
+class Calculation
 {
     private:
-    T num1,num2;
+    T num1,num2,num3;
     public:
-    Addition(T a,T b)
+    Calculation(T a,T b)
     {
+       T c;
        num1=a;
        num2=b;
+       num3=c;
     }
     T add()
     {
-        T num3;
         num3=num1+num2;
+        return num3;
+    }
+    T sub()
+    {
+        num3=num1-num2;
+        return num3;
+    }
+    T mul()
+    {
+        num3=num1*num2;
         return num3;
     }
 };
 int main()
 {
-    Addition <int> a1(1,2);
-    Addition <float>a2(2.4,4.5);
+    Calculation <int> a1(1,2);
+    Calculation <float>a2(2.4,4.5);
     cout<<"the sum of integers is:"<<a1.add()<<endl;
     cout<<"the sum of float nos is:"<<a2.add()<<endl;
+    cout<<"the diff of integers is:"<<a1.sub()<<endl;
+    cout<<"the dif of float nos is:"<<a2.sub()<<endl;
+    cout<<"the product of integers is:"<<a1.mul()<<endl;
+    cout<<"the product of float nos is:"<<a2.mul()<<endl;
 }
