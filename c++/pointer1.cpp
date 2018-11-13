c50541b4b0523c642848b7280056915e44db7338
CodeExplorer <pmcreddy@gmail.com>
2018-11-13 09:28:54 +0530

    formated

--- c++/pointer1.cpp
+++ c++/pointer1.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int a,*p;
-    p=&a;
-    cout<<"enter the value of a:";
-    cin>>a;
-    cout<<"the address of a is:"<<p<<endl;
-    p++; 
-    cout<<"incremented address value:"<<p<<endl;
+    int a, *p;
+    p = &a;
+    cout << "enter the value of a:";
+    cin >> a;
+    cout << "the address of a is:" << p << endl;
+    p++;
+    cout << "incremented address value:" << p << endl;
     p--;
-    cout<<"the vakue of a is:"<<*p<<endl;
-    cout<<"the address of p is:"<<&p<<endl;
-    *p=(*p)/2;
-    cout<<"the value of a when divided by 2 is:"<<*p<<endl;
+    cout << "the vakue of a is:" << *p << endl;
+    cout << "the address of p is:" << &p << endl;
+    *p = (*p) / 2;
+    cout << "the value of a when divided by 2 is:" << *p << endl;
 }
