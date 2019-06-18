e5eb8b2f57d4f2c35c18cff59e86ce880471a3f6
mridhula <mridhulasreedharan53088@gmail.com>
2019-06-18 15:44:53 +0530

    deletion

---
+++ simpleprojects/delete.py
@@ -0,0 +1,26 @@
+#!/Python27/Python.exe
+print "Content-type:text/html\n\r"
+import cgi
+formfield=cgi.FieldStorage()
+staffid=formfield.getvalue('q')
+import MySQLdb
+db=MySQLdb.connect("127.0.0.1","root","root","staff")
+if db:
+          cursor=db.cursor()
+          try:
+                     query="delete from tbl_staff where sid=%s"%(staffid)
+                     cursor.execute(query)
+                     db.commit()
+                     print "<script>location.href='fifth1.py';</script>"
+          except:
+                     db.rollback()
+          
+else:
+          print "not connected"
+
+
+
+
+
+delete.py
+Displaying second page.py.
