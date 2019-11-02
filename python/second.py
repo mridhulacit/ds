b8531b3137738bd55ef0228a05c8a861b6dc3bb8
mridhula <mridhulasreedharan53088@gmail.com>
2019-11-02 23:57:46 +0530

    adding

---
+++ python/second.py
@@ -0,0 +1,12 @@
+#!/Python27/python.exe
+print "Content-type:text/html\n\r"
+import cgi
+formfield=cgi.FieldStorage()
+name=formfield.getvalue('username')
+pwd=formfield.getvalue('password')
+if name=="LST" and pwd=="LST":
+           print "<script>location.href='third page.py'</script>"
+
+else:
+           print "<script>location.href='first page.py'</script>"
+##           print "Invalid Username or Password"
